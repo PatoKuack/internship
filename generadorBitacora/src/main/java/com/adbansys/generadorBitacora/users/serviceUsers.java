fcdf44db3f402f51081b33ef3e6e92e967da6ea3
Pato Kuack <nocturno_x_13514@hotmail.com>
2023-12-04 12:49:52 -0600

    practicando el commit desde eclipse

--- generadorBitacora/src/main/java/com/adbansys/generadorBitacora/users/serviceUsers.java
+++ generadorBitacora/src/main/java/com/adbansys/generadorBitacora/users/serviceUsers.java
@@ -1,6 +1,6 @@
 package com.adbansys.generadorBitacora.users;
 
-import java.util.ArrayList;
+// import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Optional;
